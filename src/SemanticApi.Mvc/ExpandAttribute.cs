5a2f718e3977067fe1ee8b9e5524a358e74fa013
Matt VanNuys <vannuysm@gmail.com>
2016-06-20 01:36:14 -0400

    Made ExpandAttribute actually work

--- src/SemanticApi.Mvc/ExpandAttribute.cs
+++ src/SemanticApi.Mvc/ExpandAttribute.cs
@@ -8,25 +8,16 @@ namespace SemanticApi.Mvc
     public class ExpandAttribute : ActionFilterAttribute
     {
         public override void OnActionExecuting(ActionExecutingContext context) {
-            var request = context.HttpContext.Request;
-            List<string> expansions;
+            var request = context.HttpContext?.Request;
+            var expansions = new List<string>();
             
-            if (request.Query.ContainsKey("expand")) {
+            if (request != null && request.Query.ContainsKey("expand")) {
                 var values = request.Query["expand"][0];
-                expansions = values.Split(',').ToList();
-            }
-            else {
-                expansions = new List<string>();
+                expansions.AddRange(values.Split(','));
             }
                 
-            context.ActionArguments.Add("expansions", new ReadOnlyCollection<string>(expansions));
+            context.ActionArguments["expansions"] = new ReadOnlyCollection<string>(expansions);
             base.OnActionExecuting(context);
-        }
-        
-        private ReadOnlyCollection<string> ProcessExpansions() {
-            
-            
-            return new ReadOnlyCollection<string>(new List<string>());
         }
     }
 }
